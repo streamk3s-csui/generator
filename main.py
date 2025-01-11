976495bf3cb9389d2ea8c27dcfc6804f41f4c565
rayhan randi <rayhanrandi10@gmail.com>
2025-01-12 00:18:53 +0700

    chore: test main function

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+from config.logging import Logger
+from config.utils import get_env_value, setup_env
+from models.bike import Bike
+
+
+
 if __name__ == '__main__':
-    print("hello")
+
+    setup_env()
+
+    logger = Logger().setup_logger('main_executor')
+
+    logger.info(get_env_value('GPX_DATAPATH'))
+
+    logger.info(get_env_value('MQTT_BROKER_ADDR'))    
+    logger.info(get_env_value('MQTT_BROKER_TOPIC'))    
+
+    logger.info(get_env_value('VECTOR_SINK_ADDR'))
+
+    bike = Bike(1, 100, get_env_value('GPX_DATAPATH'))    
+
