9a50db36c6c9185a4850d4140706b632ee0a36fb
rayhan randi <rayhanrandi10@gmail.com>
2025-01-13 14:02:38 +0700

    refactor(svc): remove gpx parsing

--- service/bike.py
+++ service/bike.py
@@ -9,12 +9,11 @@ import gpxpy
 import gpxpy.gpx
 
 from config.logging import Logger
-# from mqtt.publish import 
 
 
 
 class Bike:
-    def __init__(self, number: int, datapath: str) -> None:
+    def __init__(self, number: int, gpxd) -> None:
         self.id = uuid.uuid4()
         self.number: int = number
         self.battery_level: int = 100
@@ -23,8 +22,7 @@ class Bike:
         self.longitude: float = 0
         self.speed: float = 0 
         self.active: bool = False
-        self.datapath: str = datapath   # path to gpx dataset
-        self.logger: Logger = Logger().setup_logger(f'bike_{number}')
+        self.gpxd = gpxd    # parsed gpx dataset
     
 
     def start(self) -> None:
@@ -32,28 +30,16 @@ class Bike:
         Selects and parses a random `.gpx` path dataset, then traces it to simulate realtime bike activity.
         Sends trace data to MQTT broker with a delay of `latency`, defaults to 0.5 seconds.
         """
-        datasets = os.listdir(self.datapath)
-        data = datasets[random.randint(0, len(datasets) - 1)]
-        
         self.active = True
         self.battery_level = random.randint(75, 100)
-        with open(f'{self.datapath}/{data}', 'r') as gpxf:
-            gpx = gpxpy.parse(gpxf) 
             
-            for track in gpx.tracks:
-                for segment in track.segments:
-                    for point in segment.points:
-                        self.logger.info(f'[TRACK] Point at ({point.latitude},{point.longitude}) -> {point.elevation}')
-                        self.publish(point.latitude, point.longitude, point.elevation)
-            
-            for waypoint in gpx.waypoints:
-                self.logger.info(f'[WAYPOINT] Waypoint {waypoint.name} -> ({waypoint.latitude},{waypoint.longitude})')
-                 
-            for route in gpx.routes:
-                self.logger.info('[ROUTE] Route: ')
-                for point in route.points:
-                    self.logger.info(f'[ROUTE] Point at ({point.latitude},{point.longitude}) -> {point.elevation}')
+        # for track in gpx.tracks:
+        #     for segment in track.segments:
+        #         for point in segment.points:
+        #             # self.logger.info(f'[TRACK] Point at ({point.latitude},{point.longitude}) -> {point.elevation}')
+        #             self.publish(point.latitude, point.longitude, point.elevation)
 
+        self.finish()   
 
     def finish(self) -> None:
         raise NotImplementedError
@@ -63,16 +49,8 @@ class Bike:
         """
         Publish a single trace data point to MQTT broker with simulated latency.
         """
-        # probability distribution k:v
-        # k = latency, v = probability
-        dist = {
-            0.1     : 0.3,
-            0.25    : 0.1,
-            0.5     : 0.2,
-            0.75    : 0.1, 
-            1       : 0.3 
-        }
-        latency = random.choice(list(dist.keys()), list(dist.values()))
+        # simulate latency between 0.1 and 0.5 secs with equal probability
+        latency = random.choice([0.1, 0.2, 0.3, 0.4, 0.5], [0.2 for _ in range(5)])
         message = {
             "bike_id": str(self.id),
             "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
@@ -88,6 +66,7 @@ class Bike:
             }
         }
         time.sleep(latency)
+        # publish to mqtt
 
     
 
